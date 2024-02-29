8df7c93a993aeb369646550f2497e996b82d7fd5
mateoleont <mateolt505@gmail.com>
2024-02-28 19:17:26 -0500

    app

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>HOLA😂🤣😒</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'flex-end',
+//     justifyContent: 'center',
+//   },
+// });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  return (
+    <View 
+    style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}
+    // style={styles.container}
+    >
+      <Text style={{fontSize: 40}}>First Project</Text>
+      <Text style={{fontSize: 30}}>Using React Native👾</Text>
+      {/* <StatusBar style="auto" /> */}
+    </View>
+  );  
+}
+export default App;
+
+
+
