449d5f9973658de89fd0f2c7a80de71204fbf0ae
mateoleont <mateolt505@gmail.com>
2024-02-29 17:21:51 -0500

    ajustes

--- App.js
+++ App.js
@@ -1,26 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 const App = () => {
   return (
-    <View 
-    // style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}
-    style={styles.container}
+    <View
+      // style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}
+      style={styles.container}
     >
-      <Text style={{fontSize: 40}}>First Project</Text>
-      <Text style={{fontSize: 30}}>Using React Native👾</Text>
+      <Text style={styles.bigText}>First Project</Text>
+      <Text style={styles.text}>Using React Native👾</Text>
       {/* <StatusBar style="auto" /> */}
     </View>
-  );  
-}
+  );
+};
 export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'flex-end',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bigText: {
+    fontSize: 40,
+  },
+  text: {
+    fontSize: 30,
   },
 });
-
